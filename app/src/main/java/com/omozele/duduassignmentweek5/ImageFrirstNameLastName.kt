be1b9f75ac0a623af2c75f20d9d19ddee2a740e2
joytammy <lopatromo@yahoo.com>
2021-07-05 17:32:21 +0100

    Initial commit

---
+++ app/src/main/java/com/omozele/duduassignmentweek5/ImageFrirstNameLastName.kt
@@ -0,0 +1,11 @@
+package com.omozele.duduassignmentweek5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageFrirstNameLastName : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_frirst_name_last_name)
+    }
+}
